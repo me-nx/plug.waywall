8551a1b35e92305aac16fcd65ad0228f95601f35
me-nx <me0nx@proton.me>
2026-04-09 23:11:21 +0200

    feat: change default plugin dir to .local/state/waywall/plug

--- globals.lua
+++ globals.lua
@@ -1,6 +1,6 @@
 local M = {}
 
 M.WAYWALL_CONFIG_DIR = os.getenv("HOME") .. "/.config/waywall/"
-M.PLUG_CONFIG_DIR = os.getenv("HOME") .. "/.config/waywall/" -- Same as waywall config dir.
+M.PLUG_CONFIG_DIR = (os.getenv("XDG_STATE_HOME") or (os.getenv("HOME") .. "/.local/state")) .. "/waywall/plug/"
 
 return M
